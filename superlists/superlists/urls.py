5c5cd759c5999b335f5e9e7c607a79bc0988b09e
song geunbong <gbsong@xcs.co.kr>
2024-03-05 09:45:14 +0900

    첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import lists 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^$', lists.views.home_page, name='home')
+    # path('admin/', admin.site.urls),
 ]
