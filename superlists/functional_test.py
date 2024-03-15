287f4a866123eea1947e41685a6f7a213ae8da6d
song geunbong <gbsong@xcs.co.kr>
2024-03-15 09:35:08 +0900

    템플릿을 이용한 멘인 페이지 HTML 생성

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,11 +18,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn('To-Do', header_text)
 
         # 그녀는 바로 작업을 추가하기로 한다.
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
         # "공작깃털 사기" 라고 텍스트 상자에 입력한다.
@@ -32,9 +33,12 @@ class NewVisitorTest(unittest.TestCase):
         # "1: 공작깃털 사기" 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows),)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다"
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다.(에디스는 매우 체계적인 사람이다.)
